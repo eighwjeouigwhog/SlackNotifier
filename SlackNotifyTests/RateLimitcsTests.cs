13c5715c7f1e398ee72e72cd57b27afdc7ef237b
asuka <skuldie+github@gmail.com>
2022-12-17 15:00:13 +0900

    改造前

---
+++ SlackNotifyTests/RateLimitcsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace tverrist.Tests
+{
+    [TestClass()]
+    public class RateLimitcsTests
+    {
+        [TestMethod()]
+        public void InvokeTest()
+        {
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            var rl = new Slack.RateLimit(1000);
+
+            rl.Invoke();
+            rl.Invoke();
+            rl.Invoke();
+            rl.Invoke();
+            rl.Invoke();
+
+
+            var time = sw.Elapsed.TotalSeconds;
+            Console.WriteLine($"経過時間: {time}s");
+            Assert.IsTrue( time >= 5);
+
+        }
+    }
+}
