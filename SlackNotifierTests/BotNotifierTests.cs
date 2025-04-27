b71fe4bfe102db607b4c6e1a21ef41c011f42af1
asuka <skuldie+github@gmail.com>
2025-04-27 19:49:22 +0900

    日数対応

--- SlackNotifierTests/BotNotifierTests.cs
+++ SlackNotifierTests/BotNotifierTests.cs
@@ -1,14 +1,14 @@
 ﻿
-namespace Slack.Tests
-{
-    [TestClass()]
-    public class BotNotifierTests
-    {
-        [TestMethod()]
-        public void PushTest()
-        {
-            BotNotifier Slack = new("xoxb-587752679794-2694306191602-CTAw8GknNt9wdHp4RUU0mYk1");
-            Slack.Push(new Payload() { text = "テスト", channel="#general"});
-        }
-    }
-}
+//namespace Slack.Tests
+//{
+//    [TestClass()]
+//    public class BotNotifierTests
+//    {
+//        [TestMethod()]
+//        public void PushTest()
+//        {
+//            BotNotifier Slack = new("xoxb-587752679794-2694306191602-CTAw8GknNt9wdHp4RUU0mYk1");
+//            Slack.Push(new Payload() { text = "テスト", channel="#general"});
+//        }
+//    }
+//}
